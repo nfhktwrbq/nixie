661d0900597ddf1cce405a38f2ad65666c8e68ed
nfhktwrbq <nfhktwrbq@yandex.ru>
2024-07-07 20:07:54 +0300

    RTC and clock init. Configs for с6 and c8

--- v1/code/src/sys_init.c
+++ v1/code/src/sys_init.c
@@ -1,10 +1,11 @@
 #include <stdint.h>
 #include "sys_init.h"
+#include "stm32f103xb.h"
 
 
 // Register definitions for STM32F103C8T6
-#define RCC_BASE   0x40021000
-#define GPIOC_BASE 0x40011000
+// #define RCC_BASE   0x40021000
+// #define GPIOC_BASE 0x40011000
 
 #define RCC_APB2ENR (*(volatile uint32_t *)(RCC_BASE + 0x18))
 #define GPIOC_CRH   (*(volatile uint32_t *)(GPIOC_BASE + 0x04))
@@ -26,8 +27,50 @@ void toggle_gpio_pc13(void)
     GPIOC_ODR ^= (1 << 13);
 }
 
+static void SystemClockConfig(void)
+{
+    // Enable HSE
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY));
+
+    // Configure PLL
+    RCC->CFGR &= ~RCC_CFGR_PLLSRC; // Set PLL source to HSE
+    RCC->CFGR |= RCC_CFGR_PLLMULL9; // Set PLL multiplication factor to 9
+    RCC->CR |= RCC_CR_PLLON; // Enable PLL
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+
+    // Select PLL as system clock source
+    RCC->CFGR &= ~RCC_CFGR_SW;
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+
+    // Enable to write to RCC->BDCR
+    PWR->CR |= PWR_CR_DBP;
+
+    // Configure RTC clock source to LSE
+    RCC->BDCR |= RCC_BDCR_RTCSEL_0;
+
+    // Enable RTC
+    RCC->BDCR |= RCC_BDCR_RTCEN;
+
+    // Enable LSE
+    RCC->BDCR |= RCC_BDCR_LSEON;
+    while (!(RCC->BDCR & RCC_BDCR_LSERDY));
+
+    PWR->CR &= ~PWR_CR_DBP;
+
+    // Configure peripheral clocks
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2; // APB1 clock = HCLK/2
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV1; // APB2 clock = HCLK
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1; // AHB clock = SYSCLK
+}
+
 void SystemInit(void)
 {
+    SystemClockConfig();
+
     init_gpio_pc13();
     volatile uint32_t i = 50;
     for (uint32_t k = 0; k < 10; k++)
